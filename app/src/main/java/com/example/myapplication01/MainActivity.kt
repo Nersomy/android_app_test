603bfd521c8b4a70562239619f92af841f72bcc5
nerso <nersomy@gmail.com>
2023-05-11 22:29:21 +0200

    1

--- app/src/main/java/com/example/myapplication01/MainActivity.kt
+++ app/src/main/java/com/example/myapplication01/MainActivity.kt
@@ -7,14 +7,47 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // здесь происходит инициализация активности и создание пользовательского интерфейса
     }
-//vfbfdvfv
     override fun onStart() {
         super.onStart()
+        // активность становится видимой, но еще не готова для взаимодействия с пользователем
     }
 
     override fun onResume() {
         super.onResume()
+        // активность становится активной и готова для взаимодействия с пользователем
     }
+
+    override fun onPause() {
+        super.onPause()
+        // активность теряет фокус, но остается видимой на экране
+    }
+
+    override fun onStop() {
+        super.onStop()
+        // активность становится невидимой для пользователя
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // активность уничтожается и освобождаются все связанные с ней ресурсы
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        // активность перезапускается после того, как была остановлена, но не уничтожена
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        // сохранение данных, необходимых для восстановления состояния активности
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        // восстановление сохраненных данных
+    }
+
 
 }
